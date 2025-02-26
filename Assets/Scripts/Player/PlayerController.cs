233d645d4644fd7d6bea374f77cdccc9b89a2cc0
Khanh Nguyen <khanhsoi546@gmail.com>
2025-02-27 03:06:32 +0700

    add gun to player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,27 +9,49 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 15f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
+
+    [SerializeField] private ObjectPool bulletPool;  // Reference to your bullet object pool
+    [SerializeField] private Transform firePoint;    // Where bullets spawn
+    [SerializeField] private float fireRate = 0.5f; // Time between shots
+    private float nextFireTime = 0f;
+    private bool facingRight = true;
     private Animator animator;
     private bool isGrounded;
     private Rigidbody2D rb;
-    //private GameManager gameManager;
-    //private AudioManager audioManager;
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        //gameManager = FindAnyObjectByType<GameManager>();
-        //audioManager = FindAnyObjectByType<AudioManager>();
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        //if (gameManager.IsGameOver() || gameManager.IsGameWin()) return;
+        if (Input.GetKeyDown(KeyCode.J) && Time.time > nextFireTime)  // Fire with J
+        {
+            FireBullet();
+            nextFireTime = Time.time + fireRate;
+        }
         HandleMovement();
         HandleJump();
         UpdateAnimation();
     }
+
+    private void FireBullet()
+    {
+        GameObject bullet = bulletPool.GetObject();  // Get a bullet from the pool
+        bullet.transform.position = firePoint.position;
+        bullet.transform.rotation = Quaternion.identity;
+        bullet.SetActive(true);
+
+        PlayerBullet bulletComponent = bullet.GetComponent<PlayerBullet>();
+        if (bulletComponent != null)
+        {
+            Vector2 direction = facingRight ? Vector2.right : Vector2.left;  // Fire in the correct direction
+            bulletComponent.Launch(direction);
+        }
+    }
+
     private void HandleMovement()
     {
         float moveInput = Input.GetAxis("Horizontal");
@@ -40,7 +63,6 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            //audioManager.PlayJumpSound();
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
@@ -48,10 +70,6 @@ public class PlayerController : MonoBehaviour
     }
     private void UpdateAnimation()
     {
-        if (!isGrounded)
-        {
-            Debug.Log("dsfsdfs");
-        }
         bool isRunning = Mathf.Abs(rb.velocity.x) > 0.1f;
         bool isJumping = !isGrounded;
         animator.SetBool("isRunning", isRunning);
