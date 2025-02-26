e1878bf77387f27aa7d1b1a842f013c786e5ee43
sannxt <sannxt244@gmail.com>
2025-02-27 02:19:10 +0700

    ntsan add boss

---
+++ Assets/Scripts/Common/BossArea.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BossArea : MonoBehaviour
+{
+    public bool isPlayerInside = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInside = false;
+        }
+    }
+}
