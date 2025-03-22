cdea21b909b8feffb8242ee4de479beb856f34be
Vi Nguyen <xuanvidz03102003@gmail.com>
2025-03-23 00:07:51 +0700

    add  more case game over

---
+++ Assets/Scripts/Player/AmmoSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AmmoSpawner : MonoBehaviour
+{
+    public GameObject ammoPackPrefab; // Gán Prefab AmmoPack trong Inspector
+    public Transform player; // Gán Object Player vào đây
+    public float spawnInterval = 15f; // Thời gian spawn
+    public float spawnHeight = 5f; // Khoảng cách rơi từ trên xuống
+
+    private void Start()
+    {
+        StartCoroutine(SpawnAmmoPack());
+    }
+
+    IEnumerator SpawnAmmoPack()
+    {
+        while (true) // Vòng lặp vô hạn để luôn spawn AmmoPack
+        {
+            yield return new WaitForSeconds(spawnInterval); // Chờ 15s
+            Spawn();
+            Debug.Log("spaw Ammo sucess");
+
+        }
+    }
+
+    void Spawn()
+    {
+        if (player != null && ammoPackPrefab != null)
+        {
+            Vector3 spawnPosition = new Vector3(player.position.x, player.position.y + spawnHeight, 0);
+            Instantiate(ammoPackPrefab, spawnPosition, Quaternion.identity);
+            Debug.Log("spaw sucess");
+        }
+    }
+}
--- Assets/Scripts/Scene/SceneController.cs
+++ Assets/Scripts/Scene/SceneController.cs
@@ -12,25 +12,36 @@ public class SceneController : MonoBehaviour
             PlayerPrefs.SetInt("GameInitialized", 1); // Đánh dấu game đã được khởi động lần đầu
             PlayerPrefs.Save();
         }
-
-      
+        else
+        {
+            // Nếu game đã từng chạy trước đó, nhưng LastPlayedMap bị mất thì đặt lại mặc định
+            if (!PlayerPrefs.HasKey("LastPlayedMap"))
+            {
+                PlayerPrefs.SetString("LastPlayedMap", "Island");
+                PlayerPrefs.Save();
+            }
+        }
     }
+  
+
     private void ResetGameData()
     {
         PlayerPrefs.DeleteAll(); // Xóa toàn bộ dữ liệu cũ
 
         // Thiết lập giá trị mặc định
         PlayerPrefs.SetInt("Coin", 0);
-        PlayerPrefs.SetString("LastPlayedMap", "Island"); // Mặc định bắt đầu ở Island
-        PlayerPrefs.SetInt("MaxAmmo", 10); // Nếu có max đạn, đặt giá trị mặc định
+        PlayerPrefs.SetString("LastPlayedMap", "Island");
+        PlayerPrefs.SetInt("MaxAmmo", 10);
         PlayerPrefs.Save();
 
-        Debug.Log("Game data has been reset to default!");
+        Debug.Log("Game data has been reset! LastPlayedMap = " + PlayerPrefs.GetString("LastPlayedMap"));
     }
+
 
     public void LoadGameScene()
     {
         string lastMap = PlayerPrefs.GetString("LastPlayedMap", "Island"); // Mặc định là Island nếu chưa có dữ liệu
+        Debug.Log("Loading map: " + lastMap);
         SceneManager.LoadScene(lastMap);
     }
     
