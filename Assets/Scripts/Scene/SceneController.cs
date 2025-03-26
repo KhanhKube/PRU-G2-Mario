b8eac1057e3bf2d15d2418a31a1ef90393026ca0
Vi Nguyen <xuanvidz03102003@gmail.com>
2025-03-27 00:29:17 +0700

    complete game _beta test

--- Assets/Scripts/Scene/SceneController.cs
+++ Assets/Scripts/Scene/SceneController.cs
@@ -5,20 +5,24 @@ public class SceneController : MonoBehaviour
 {
     private void Awake()
     {
-        // Xóa toàn bộ dữ liệu cũ mỗi khi game chạy lại
-        PlayerPrefs.DeleteAll();
-
-        // Thiết lập giá trị mặc định
-        PlayerPrefs.SetInt("Coin", 0);
-        PlayerPrefs.SetString("LastPlayedMap", "Island");
-        PlayerPrefs.SetInt("MaxAmmo", 10);
-        PlayerPrefs.Save();
-
-        Debug.Log("Game has been reset! LastPlayedMap = " + PlayerPrefs.GetString("LastPlayedMap"));
+        // Kiểm tra nếu game vừa khởi động lần đầu (chưa có dữ liệu)
+        if (!PlayerPrefs.HasKey("GameInitialized"))
+        {
+            ResetGameData(); // Reset về trạng thái ban đầu
+            PlayerPrefs.SetInt("GameInitialized", 1); // Đánh dấu game đã được khởi động lần đầu
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            // Nếu game đã từng chạy trước đó, nhưng LastPlayedMap bị mất thì đặt lại mặc định
+            if (!PlayerPrefs.HasKey("LastPlayedMap"))
+            {
+                PlayerPrefs.SetString("LastPlayedMap", "Island");
+                PlayerPrefs.Save();
+            }
+        }
     }
-
-
-    private void ResetGameData()
+    public void ResetGameData()
     {
         PlayerPrefs.DeleteAll(); // Xóa toàn bộ dữ liệu cũ
 
@@ -29,6 +33,7 @@ public class SceneController : MonoBehaviour
         PlayerPrefs.Save();
 
         Debug.Log("Game data has been reset! LastPlayedMap = " + PlayerPrefs.GetString("LastPlayedMap"));
+        SceneManager.LoadScene("MainScene");
     }
 
 
